ca7d450133e3850f42f1645932a50e04be01d74b
rsquaredin <rsquaredin@gmail.com>
2022-08-24 19:14:11 +0530

    fix: global variables

--- R/yahoofinancer.R
+++ R/yahoofinancer.R
@@ -2,6 +2,14 @@
 #' 
 #' Fetch Data from Yahoo Finance API
 #' 
+#' See the README on
+#' \href{https://github.com/rsquaredacademy/yahoofinancer}{GitHub}
+#' 
 #' @docType package
 #' @name yahoofinancer
 NULL
+
+if (getRversion() >= "2.15.1") {
+  utils::globalVariables(c(".", "adjclose", "chart", "currencies", "finance",
+   "marketSummaryResponse", "quoteResponse", "result", "volume"))
+}
