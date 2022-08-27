36abd07a6872ceb34311a5279ba648dc1c3f44c7
rsquaredin <rsquaredin@gmail.com>
2022-08-27 10:19:44 +0530

    test: index history

--- tests/testthat/test-index.R
+++ tests/testthat/test-index.R
@@ -12,3 +12,21 @@ httptest::with_mock_api({
                  c(15752.05, 15835.35, 15810.85, 15989.80, 16132.90, 16220.60))
   })
 })
+
+
+httptest::with_mock_api({
+  test_that("output from index summary is as expected", {
+    testthat::skip_on_cran()
+    nifty <- Index$new('^NSEI')
+    trend <- nifty$summary_detail
+    expect_equal(length(trend), 54)
+    expect_equal(trend$symbol, "^NSEI")
+    expect_equal(trend$quoteType, "INDEX")
+    expect_equal(trend$currency, "INR")
+    expect_equal(trend$regularMarketPrice, 17558.9)
+    expect_equal(trend$shortName, "NIFTY 50")
+    expect_equal(trend$regularMarketDayHigh, 17685.85)
+    expect_equal(trend$regularMarketDayLow, 17519.35)
+    expect_equal(trend$fullExchangeName, "NSE")
+  })
+})
