0e2a5471ab41ec66789e22bf621a36b707a8ba7e
rsquaredin <rsquaredin@gmail.com>
2022-08-30 17:07:27 +0530

    test: balance sheet data

---
+++ tests/testthat/test-ticker.R
@@ -0,0 +1,25 @@
+httptest::with_mock_api({
+  test_that("output from ticker balance sheet annual is as expected", {
+    testthat::skip_on_cran()
+    aapl <- Ticker$new('aapl')
+    trend <- aapl$get_balance_sheet('annual')
+
+    expect_equal(nrow(trend), 4)
+    expect_equal(ncol(trend), 24)
+    expect_equal(trend$end_date,
+                 as.Date(c("2021-09-25", "2020-09-26", "2019-09-28", "2018-09-29")))
+  })
+})
+
+httptest::with_mock_api({
+  test_that("output from ticker balance sheet quarterly is as expected", {
+    testthat::skip_on_cran()
+    aapl <- Ticker$new('aapl')
+    trend <- aapl$get_balance_sheet('quarter')
+
+    expect_equal(nrow(trend), 4)
+    expect_equal(ncol(trend), 24)
+    expect_equal(trend$end_date,
+                 as.Date(c("2022-06-25", "2022-03-26", "2021-12-25", "2021-09-25")))
+  })
+})
