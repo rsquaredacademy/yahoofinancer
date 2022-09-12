02dde177a1ceae5a34a5f2c77a600e1bd848adf1
rsquaredin <rsquaredin@gmail.com>
2022-09-12 10:26:45 +0530

    docs: update

--- R/index.R
+++ R/index.R
@@ -1,9 +1,9 @@
 #' R6 Class Representing a Ticker
 #' 
 #' @description 
-#' Base class for getting all data related to indices from Yahoo Finance API
+#' Base class for getting all data related to indices from Yahoo Finance API.
 #' 
-#' @param index Index for which data has to be retrieved
+#' @param index Index for which data has to be retrieved.
 #' 
 #' @docType class
 #' @format An R6 class object
@@ -49,16 +49,46 @@ Index <- R6::R6Class(
 
     #' @description 
     #' Retrieves historical data
-    #' @param period Length of time
-    #' @param interval Time between data points
-    #' @param start Specific starting date
-    #' @param end Specific ending date
+    #' @param period Length of time. Defaults to \code{'ytd'}. Valid values are:
+    #' \itemize{
+    #' \item \code{'1d'}
+    #' \item \code{'5d'}
+    #' \item \code{'1mo'}
+    #' \item \code{'3mo'}
+    #' \item \code{'6mo'}
+    #' \item \code{'1y'}
+    #' \item \code{'2y'}
+    #' \item \code{'5y'}
+    #' \item \code{'10y'}
+    #' \item \code{'ytd'}
+    #' \item \code{'max'}
+    #' }
+    #' @param interval Time between data points. Defaults to \code{'1d'}. Valid values are:
+    #' \itemize{
+    #' \item \code{'1m'}
+    #' \item \code{'2m'}
+    #' \item \code{'5m'}
+    #' \item \code{'15m'}
+    #' \item \code{'30m'}
+    #' \item \code{'60m'}
+    #' \item \code{'90m'}
+    #' \item \code{'1h'}
+    #' \item \code{'1d'}
+    #' \item \code{'5d'}
+    #' \item \code{'1wk'}
+    #' \item \code{'1mo'}
+    #' \item \code{'3mo'}
+    #' }
+    #' @param start Specific starting date.
+    #' @param end Specific ending date.
+    #' @return A \code{data.frame}.
     #' @examples
+    #' \donttest{
     #' nifty <- Index$new('^NSEI')
-    #' nifty$get_history()
     #' nifty$get_history(start = '2022-07-01', interval = '1d')
     #' nifty$get_history(start = '2022-07-01', end = '2022-07-14', interval = '1d')
     #' nifty$get_history(period = '1mo', interval = '1d')
+    #' }
     get_history = function(period = 'ytd', interval = '1d', start = NULL, end = NULL) {
 
       if (!is.null(start)) {
