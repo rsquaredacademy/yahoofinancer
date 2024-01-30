52d34f85a7eec353599a4436d7a6e5ae92b0b264
aravindhebbali <hebbali.aravind@gmail.com>
2024-01-30 16:15:09 +0530

    chore: closes #6

--- R/yahoofinancer.R
+++ R/yahoofinancer.R
@@ -1,14 +1,15 @@
 #' \code{yahoofinancer} package
-#' 
+#'
 #' Fetch Data from Yahoo Finance API
-#' 
+#'
 #' See the README on
 #' \href{https://github.com/rsquaredacademy/yahoofinancer}{GitHub}
-#' 
+#'
 #' @docType package
 #' @keywords internal
 #' @name yahoofinancer
-NULL
+#' @aliases yahoofinancer-package
+"_PACKAGE"
 
 if (getRversion() >= "2.15.1") {
   utils::globalVariables(c(".", "adjclose", "chart", "currencies", "finance",
